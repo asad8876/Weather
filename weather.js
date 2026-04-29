2a4bde71999086c93122b97e301cad33c84cb292
Asad Abbas Abidi <asadofficial4455@gmail.com>
2026-04-30 03:28:26 +0530

    showWeather function and showError function has been created to display the current location atmosphere of the device and display error if not get the coordinated

--- weather.js
+++ weather.js
@@ -1,6 +1,7 @@
+const apiKey = "0196eb753b32483ebb3144743262904";
+
 function getWeather() {
   const city = document.getElementById("city").value.trim();
-  const apiKey = "0196eb753b32483ebb3144743262904";
 
 
   const url = `https://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${city}`;
@@ -12,15 +13,15 @@ function getWeather() {
       const location = data.location.name;
       const tempC = data.current.temp_c;
       const tempF = data.current.temp_f;
-      const condition = data.current.condition.text;
+      const atmosphere = data.current.condition.text;
       const windM = data.current.wind_mph;
       const windK = data.current.wind_kph;
 
       document.getElementById("result").innerHTML =
-        `Location: ${location}
+        `Location: ${location} <br>
         Temperature: ${tempC}°C | ${tempF}F <br> 
-        Condition: ${condition} <br> 
-        Wind: ${windM} | ${windK}`;
+        Atmosphere: ${atmosphere} <br> 
+        Wind: ${windM} Mph | ${windK} Kph`;
     })
     .catch(error => {
       console.log("Error:", error);
@@ -28,3 +29,46 @@ function getWeather() {
 }
 
     document.getElementById("button").addEventListener("click", getWeather);
+
+
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(showWeather, showError);
+    } else {
+      document.getElementById("output").innerHTML =
+        "Geolocation not supported.";
+    }
+
+
+    function showWeather(position) {
+      const lat = position.coords.latitude;
+      const lon = position.coords.longitude;
+
+      const url = `https://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${lat},${lon}`;
+
+      fetch(url)
+        .then(response => response.json())
+        .then(data => {
+          
+          const location = data.location.name;
+          const tempC = data.current.temp_c;
+          const tempF = data.current.temp_f;
+          const atmosphere = data.current.condition.text;
+          const windM = data.current.wind_mph;
+          const windK = data.current.wind_kph;
+
+          document.getElementById("output").innerHTML =
+            `Location: ${location} <br>
+             Temperature: ${tempC}°C | ${tempF}F <br> 
+             Atmosphere: ${atmosphere} <br> 
+             Wind: ${windM} Mph | ${windK} Kph`;
+        })
+        .catch(err => {
+          document.getElementById("output").innerHTML = "Error fetching weather.";
+          console.error(err);
+        });
+    }
+
+    function showError() {
+      document.getElementById("output").innerHTML =
+        "Location access denied.";
+    }
